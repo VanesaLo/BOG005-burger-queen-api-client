946c54f64f0532c8f0314483eee217259968e1f7
LuisacnyBaudin <luisacbaudin@gmail.com>
2022-11-15 10:48:48 -0500

    comenzando a probar react

---
+++ burguer-queen/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Paginainicial from './componentes/vistainicial';
+
+
+function App(){
+  return (
+   <div className='App'>
+  
+    <h1>Inicio de sesión</h1>
+    < Paginainicial />
+    
+   </div>
+  );
+}
+
+export default App
