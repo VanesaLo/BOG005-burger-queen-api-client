45f08a1a6fd0c3faa0412dc2c312f35263e01903
LuisacnyBaudin <luisacbaudin@gmail.com>
2022-11-17 14:23:16 -0500

    conectando la API y utilizando Hooks

--- burguer-queen/src/components/login.jsx
+++ burguer-queen/src/components/login.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import "../styles/login.css";
 import image from "../images/Gourmet.jpg";
-import arrow from '../images/angle-left.png';
+import { loginUsers } from "./utilis/peticiones";
+import { useState } from "react";
 
+ 
 function Login() {
+  const handledSubmit = (e) => {
+    e.preventDefault(); 
+  }
+   const [email, setPassword]= useState(" ");
+   const [setEmail, Password]= useState(" "); 
+ 
+  function handleChange(event) {
+    console.log(event.target.value);
+  }
   return (
     <div className="container">
       <section>
         <img className="logoImage" src={image} alt="foto del logo" />
       </section>
-
+      
       <section className="containerForm">
       <h2 className="TitleForm"> Iniciar Sesión</h2>
-        <form className="formLogin">
+        <form className="formLogin" onSubmit={handledSubmit}>
          <label>Correo</label>
-            <input className="emailUser" type="email" placeholder="Ingrese correo" />
+            <input className="emailUser" type="email" placeholder="Ingrese correo" onChange={handleChange}/>
           <label>Contraseña</label>
-            <input className="passwordUser" type="password" placeholder="Ingrese contraseña" />
+            <input className="passwordUser" type="password" placeholder="Ingrese contraseña" onChange={handleChange}/>
             <h6 className="password" >¿Olvidaste tu contraseña?</h6>
-          <button className="nextButton"> Siguiente</button>
+          <button className="nextButton" type="submit"> Siguiente</button>
         </form>
       </section>
     </div>
