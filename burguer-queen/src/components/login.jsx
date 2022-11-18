a5ec4e2f8dd5db85842a4c21ca4504b867628256
MODAOZUSH <yeimmy.lopez272@gmail.com>
2022-11-18 09:49:09 -0500

    agregando hook de estado para el email y contraseña

--- burguer-queen/src/components/login.jsx
+++ burguer-queen/src/components/login.jsx
@@ -1,35 +1,56 @@
 import React from "react";
 import "../styles/login.css";
 import image from "../images/Gourmet.jpg";
-import { loginUsers } from "./utilis/peticiones";
+import { loginUsers } from "../utils/petitions";
 import { useState } from "react";
 
- 
 function Login() {
   const handledSubmit = (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
+    console.log(email, password);
+  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // function handleChangeEmail(event) {
+  //   setEmail(event.target.value);
+  // }
+  // // console.log(email);
+
+  function handleChangePass(event) {
+    setPassword(event.target.value);
   }
-   const [email, setPassword]= useState(" ");
-   const [setEmail, Password]= useState(" "); 
- 
-  function handleChange(event) {
-    console.log(event.target.value);
-  }
+
   return (
     <div className="container">
       <section>
         <img className="logoImage" src={image} alt="foto del logo" />
       </section>
-      
+
       <section className="containerForm">
-      <h2 className="TitleForm"> Iniciar Sesión</h2>
+        <h2 className="TitleForm"> Iniciar Sesión</h2>
         <form className="formLogin" onSubmit={handledSubmit}>
-         <label>Correo</label>
-            <input className="emailUser" type="email" placeholder="Ingrese correo" onChange={handleChange}/>
+          <label>Correo</label>
+          <input
+            className="emailUser"
+            value={email}
+            type="email"
+            placeholder="Ingrese correo"
+            onChange={(e)=>setEmail(e.target.value)}
+          />
           <label>Contraseña</label>
-            <input className="passwordUser" type="password" placeholder="Ingrese contraseña" onChange={handleChange}/>
-            <h6 className="password" >¿Olvidaste tu contraseña?</h6>
-          <button className="nextButton" type="submit"> Siguiente</button>
+          <input
+            className="passwordUser"
+            value={password}
+            type="password"
+            placeholder="Ingrese contraseña"
+            onChange={handleChangePass}
+          />
+          <h6 className="password">¿Olvidaste tu contraseña?</h6>
+          <button className="nextButton" type="submit">
+            {" "}
+            Siguiente
+          </button>
         </form>
       </section>
     </div>
