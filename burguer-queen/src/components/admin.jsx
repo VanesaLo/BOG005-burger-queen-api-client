4a6305de0c6cc7cfb1ab2ab58e053dc9ddcf80ec
LuisacnyBaudin <luisacbaudin@gmail.com>
2022-11-21 19:31:03 -0500

    trabajando en la vista Admin2

--- burguer-queen/src/components/admin.jsx
+++ burguer-queen/src/components/admin.jsx
@@ -25,7 +25,7 @@ function Admin() {
         </header>
         
 
-        <table>
+        <table className="tablaAdmin">
         <thead>
          <tr>
          <th> Nombre </th>
@@ -33,8 +33,9 @@ function Admin() {
          <th> Status </th>
          </tr>
          <tr>
-         <td> Hola </td>
-         <td> como estas? </td>
+         <td> Andres </td>
+         <td> Mesero </td>
+         <td> Activo </td>
          </tr>
          </thead>
          </table>
