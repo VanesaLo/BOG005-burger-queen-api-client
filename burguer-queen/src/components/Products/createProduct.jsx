007ce58b325411b8b0b390befd7a642d37e25956
LuisacnyBaudin <luisacbaudin@gmail.com>
2023-01-31 19:20:18 -0500

    trabajando en los modales de bootstrap

--- burguer-queen/src/components/Products/createProduct.jsx
+++ burguer-queen/src/components/Products/createProduct.jsx
@@ -1,37 +1,18 @@
 import React, { useState } from "react";
 import "../../Modals/modal.css";
-import useModal from "../../Modals/useModal";
 import { createProducts } from "../../utils/petitions";
 import Swal from "sweetalert2";
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
-function StaticExample() {
-  return (
-    <div
-      className="modal show"
-      style={{ display: 'block', position: 'initial' }}
-    >
-      <Modal.Dialog>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>
-          <p>Modal body text goes here.</p>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button variant="secondary">Close</Button>
-          <Button variant="primary">Save changes</Button>
-        </Modal.Footer>
-      </Modal.Dialog>
-    </div>
-  );
-}
 
 function CreateProduct({admiGetProduct}) {
-  const [isOpenModal, openModal, closeModal] = useModal(false);
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const [newProduct, setNewProduct] = useState([
     { name: "", price: "", type: "" },
@@ -52,7 +33,7 @@ function CreateProduct({admiGetProduct}) {
         if (res.status === 201) {
           Swal.fire("Good job!", "You clicked the button!", "success");
         }
-        closeModal;
+      
       })
       .catch((error) => {
         console.log(error);
@@ -60,30 +41,90 @@ function CreateProduct({admiGetProduct}) {
       });
   }
 
-  return (
-    <div className="containerCreateProduct">
-      <h3 className="titleCreateUser">¿Nuevo Producto?</h3>
-      <button className="btnCreateUser" onClick={openModal}>
-        Crear producto
-      </button>
-      <div className={`modal ${isOpenModal && "modal-open"}`}>
-        <form className="formModal" onSubmit={handleSubmit}>
-          <button className="closeModal" onClick={closeModal}>
-            X
-          </button>
-          <h1>Crear Producto</h1>
-          <label>Nombre</label>
-          <input placeholder="" name="name" onChange={handleChange} />
-          <label>Precio</label>
-          <input placeholder="" name="price" onChange={handleChange} />
-          <label>Imagen</label>
-          <input placeholder="" />
-          <label>Tipo</label>
-          <input placeholder="" name="type" onChange={handleChange} />
-          <button type="submit">Finalizar</button>
-        </form>
-      </div>
-    </div>
+
+    return (
+      <>
+         <h3 className="titleCreateUser">¿Nuevo Producto?</h3>
+        <Button className="btnCreateUser" onClick={handleShow}> Crear producto</Button>
+   
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Crear Producto</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder=""
+                  onChange={handleChange}
+                  autoFocus
+                />
+                <Form.Label> Precio</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder=""
+                  onChange={handleChange}
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput2"
+              >
+                <Form.Label>Imagen</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder=""
+                  onChange={handleChange}
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput3"
+              >
+                <Form.Label>Tipo</Form.Label>
+                <Form.Control
+                  type="text"
+                  onChange={handleChange}
+                  autoFocus
+                />
+              </Form.Group>
+            
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={handleSubmit}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    // <div className="containerCreateProduct">
+    //   <h3 className="titleCreateUser">¿Nuevo Producto?</h3>
+    //   <button className="btnCreateUser" onClick={openModal}>
+    //     Crear producto
+    //   </button>
+    //   <div className={`modal ${isOpenModal && "modal-open"}`}>
+    //     <form className="formModal" onSubmit={handleSubmit}>
+    //       <button className="closeModal" onClick={closeModal}>
+    //         X
+    //       </button>
+    //       <h1>Crear Producto</h1>
+    //       <label>Nombre</label>
+    //       <input placeholder="" name="name" onChange={handleChange} />
+    //       <label>Precio</label>
+    //       <input placeholder="" name="price" onChange={handleChange} />
+    //       <label>Imagen</label>
+    //       <input placeholder="" />
+    //       <label>Tipo</label>
+    //       <input placeholder="" name="type" onChange={handleChange} />
+    //       <button type="submit">Finalizar</button>
+    //     </form>
+    //   </div>
+    // </div>
   );
 }
 
--- burguer-queen/src/components/Users/createUser.jsx
+++ burguer-queen/src/components/Users/createUser.jsx
@@ -1,76 +1,137 @@
-import React, { useState } from "react";
-import "../../Modals/modal.css";
-import useModal from "../../Modals/useModal";
 import { createUser } from "../../utils/petitions";
 import Swal from "sweetalert2";
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
 function CreateUsers({ admiGetUsers }) {
-  const [isOpenModal, openModal, closeModal] = useModal(false);
-  const [newUser, setNewUser] = useState([
-    { email: "", password: "", role: "" },
-  ]);
+
+ const [show, setShow] = useState(false);
+
+ const handleClose = () => setShow(false);
+ const handleShow = () => setShow(true);
+ // const [isOpenModal, openModal, closeModal] = useModal(false);
+ const [newUser, setNewUser] = useState([
+   { email: "", password: "", role: "" },
+ ]);
 
   function handleChange(e) {
     setNewUser({
       ...newUser,
       [e.target.name]: e.target.value,
-    });
-  }
+   });
+ }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    createUser(newUser)
-      .then((res) => {
-        admiGetUsers();
-        if (res.status === 201) {
-          Swal.fire("Good job!", "You clicked the button!", "success");
-        }
-        closeModal
-      })
-      .catch((error) => {
-        console.log(error);
-        Swal.fire("Error!", "Tenemos un problema, intenta de nuevo", "error");
-      });
-  }
+ function handleSubmit(e) {
+   e.preventDefault();
+   handleClose;
+   createUser(newUser)
+     .then((res) => {
+       admiGetUsers();
+       if (res.status === 201) {
+         Swal.fire("Good job!", "You clicked the button!", "success");
+       }
+  
+     })
+     .catch((error) => {
+       console.log(error);
+       Swal.fire("Error!", "Tenemos un problema, intenta de nuevo", "error");
+     });
+ }
 
-  return (
-    <div className="containerCreateUser">
-      <h3 className="titleCreateUser">¿Nuevo Usuario?</h3>
-      <button className="btnCreateUser" onClick={openModal}>
-        Crear usuario
-      </button>
-      <div className={`modal ${isOpenModal && "modal-open"}`}>
-        <form className="formModal" onSubmit={handleSubmit}>
-          <button className="closeModal" onClick={closeModal} type="button">
-            X
-          </button>
-          <h1>Crear Usuario</h1>
-          <label>Role</label>
-          <input
-            type="text"
-            name="role"
-            placeholder=""
-            onChange={handleChange}
-          />
-          <label>Correo</label>
-          <input
-            type="email"
-            name="email"
-            placeholder=""
-            onChange={handleChange}
-          />
-          <label>Contraseña</label>
-          <input
-            type="password"
-            name="password"
-            placeholder=""
-            onChange={handleChange}
-          />
-          <button type="submit">Finalizar</button>
-        </form>
-      </div>
-    </div>
-  );
+
+
+ return (
+   <>
+        <h3 className="titleCreateUser">¿Nuevo Usuario?</h3>
+        <Button className="btnCreateUser" onClick={handleShow}> Crear usuario</Button>
+     
+
+     <Modal show={show} onHide={handleClose}>
+       <Modal.Header closeButton>
+         <Modal.Title>Crear Usuario</Modal.Title>
+       </Modal.Header>
+       <Modal.Body>
+         <Form>
+           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+             <Form.Label>Rol</Form.Label>
+             <Form.Control
+               type="text"
+               placeholder="waiter"
+               onChange={handleChange}
+               autoFocus
+             />
+           </Form.Group>
+           <Form.Group
+             className="mb-3"
+             controlId="exampleForm.ControlInput2"
+           >
+             <Form.Label>Correo</Form.Label>
+             <Form.Control
+               type="email"
+               placeholder="name@example.com"
+               onChange={handleChange}
+               autoFocus
+             />
+           </Form.Group>
+           <Form.Group
+             className="mb-3"
+             controlId="exampleForm.ControlInput3"
+           >
+             <Form.Label>Contraseña</Form.Label>
+             <Form.Control
+               type="password"
+               onChange={handleChange}
+               autoFocus
+             />
+           </Form.Group>
+         </Form>
+       </Modal.Body>
+       <Modal.Footer>
+         <Button variant="primary" onClick={handleSubmit}>
+           Save Changes
+         </Button>
+       </Modal.Footer>
+     </Modal>
+   </>
+   /*   <div className="containerCreateUser">
+     <h3 className="titleCreateUser">¿Nuevo Usuario?</h3>
+     <button className="btnCreateUser" onClick={openModal}>
+       Crear usuario
+     </button>
+     <div className={`modal ${isOpenModal && "modal-open"}`}>
+       <form className="formModal" onSubmit={handleSubmit}>
+         <button className="closeModal" onClick={closeModal} type="button">
+           X
+         </button>
+         <h1>Crear Usuario</h1>
+         <label>Role</label>
+         <input
+           type="text"
+           name="role"
+           placeholder=""
+           onChange={handleChange}
+         />
+         <label>Correo</label>
+         <input
+           type="email"
+           name="email"
+           placeholder=""
+           onChange={handleChange}
+         />
+         <label>Contraseña</label>
+         <input
+           type="password"
+           name="password"
+           placeholder=""
+           onChange={handleChange}
+         />
+         <button type="submit">Finalizar</button>
+       </form>
+     </div>
+   </div> */
+ );
 }
 
 export { CreateUsers };
